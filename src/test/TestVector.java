ecee5447749fdb10329b099d09298dc5d9a58931
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-06-02 11:06:16 -0300

    Implemented Vector data structure.

---
+++ src/test/TestVector.java
@@ -0,0 +1,29 @@
+package test;
+
+import util.Vector;
+
+public class TestVector {
+	public static void main(String...strings) {
+		Vector<Integer> vec = new Vector<>();
+		for (int i = 0; i < 10; i++)
+			vec.append(i);
+		for (int i : vec)
+			System.out.print(i + " ");
+		System.out.println();
+		vec.remove(5);
+		for (int i : vec)
+			System.out.print(i + " ");
+		System.out.println();
+		vec.insert(5, 5);
+		for (int i : vec)
+			System.out.print(i + " ");
+		System.out.println();
+		interfaces.Iterator<Integer> iter = vec.iterator();
+		iter.next();
+		iter.insert(-1);
+		iter.next();
+		iter.remove();
+		for (int i : vec)
+			System.out.print(i + " ");
+	}
+}
