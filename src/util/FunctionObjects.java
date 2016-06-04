b7deda4f68f9657c6b3573db77fdf47093fe4b4f
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-06-04 00:37:12 -0300

    Added some useful generic function objects.

---
+++ src/util/FunctionObjects.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.util.Comparator;
+
+public class FunctionObjects {
+	
+	public static <T extends Comparable<T>> Comparator<T> less() {
+		return new Comparator<T>() {
+			@Override
+			public int compare(T o1, T o2) {
+				return o1.compareTo(o2);
+			}
+		};
+	}
+
+	public static <T extends Comparable<T>> Comparator<T> greater() {
+		return new Comparator<T>() {
+			@Override
+			public int compare(T o1, T o2) {
+				return o2.compareTo(o1);
+			}
+		};
+	}
+	
+}
