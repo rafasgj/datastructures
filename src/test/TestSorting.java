dbcf66a30ae22b5a1e5d75483d91602d40a55d41
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-09-07 12:48:04 -0300

    Modified sorting algorithms test to use lambdas and be more easily extendend to test new algorithms.

--- src/test/TestSorting.java
+++ src/test/TestSorting.java
@@ -12,244 +12,123 @@ package test;
 import algorithms.Sorting;
 
 public class TestSorting {
+	
+	interface Algorithm<T> {
+		long execute(T[] array);
+	}
+	
+	static class Sort<T> {
+		final String name;
+		final Algorithm<T> algorithm;
+		Sort(String name, Algorithm<T> algorithm) {
+			this.name = name;
+			this.algorithm = algorithm;
+		}
+	}
+	
+	private static Integer[] ordered = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+	private static Integer[] reversed = {16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+	private static Integer[] random = {14,14,8,7,2,3,16,15,10,1,9,12,6,13,4,5,11,17,17,14};
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String...args) {
-		Integer[] ordered = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-		Integer[] reversed = {16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-		Integer[] random = {14,14,8,7,2,3,16,15,10,1,9,12,6,13,4,5,11,17,17,14};
-		Integer[] array;
+		@SuppressWarnings("rawtypes")
+		Sort[] simple = {
+				new Sort<Integer>("Insertion",
+						(n)-> Sorting.insertionSort((Integer[])n)),
+				new Sort<Integer>("Bubble",
+						(n)-> Sorting.bubbleSort((Integer[])n)),
+				new Sort<Integer>("Selection",
+						(n)-> Sorting.selectionSort((Integer[])n)),
+		};
+		
+		@SuppressWarnings("rawtypes")
+		Sort[] fast = {
+				new Sort<Integer>("Quick",
+						(n)-> Sorting.quickSort((Integer[])n)),
+				new Sort<Integer>("Heap",
+						(n)-> Sorting.heapSort((Integer[])n)),
+				new Sort<Integer>("Intro",
+						(n)-> Sorting.introSort((Integer[])n)),
+				new Sort<Integer>("Merge",
+						(n)-> Sorting.mergeSort((Integer[])n)),
+		};
 
-		System.out.println(new String(new char[20]).replace("\0", "-"));
+		// Small arrays to test correctness.
+		for (Sort<Integer> p : simple)
+			testAlgorithm(p.name, p.algorithm);
+		for (Sort<Integer> p : fast)
+			testAlgorithm(p.name, p.algorithm);
+
+		// Large arrays to test speed.
+		random = startLargeTest("Large", 64);
+		for (Sort<Integer> p : simple)
+			testAlgorithmWithArray(
+					p.name,
+					random.clone(),
+					p.algorithm,
+					false
+			);
+		for (Sort<Integer> p : fast)
+			testAlgorithmWithArray(
+					p.name,
+					random.clone(),
+					p.algorithm,
+					false
+			);
 		
-		// Insertion sort.
-		System.out.println("Insertion Sort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.insertionSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.insertionSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.insertionSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Selection sort.
-		System.out.println("Selection Sort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.selectionSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.selectionSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.selectionSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Bubble sort.
-		System.out.println("Bubble Sort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.bubbleSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.bubbleSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.bubbleSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Quick sort.
-		System.out.println("Quick Sort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.quickSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.quickSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.quickSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Heap sort.
-		System.out.println("Heap Sort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.heapSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.heapSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.heapSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Merge sort.
-		System.out.println("Merge Sort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.mergeSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.mergeSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.mergeSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Intro sort.
-		System.out.println("IntroSort");
-		System.out.print("Sorting ordered: "); System.out.flush();
-		array = ordered.clone();
-		System.out.println(Sorting.introSort(array));
-		printArray(array);
-		System.out.print("Sorting reversed: "); System.out.flush();
-		array = reversed.clone();
-		System.out.println(Sorting.introSort(array));
-		printArray(array);
-		System.out.print("Sorting random: "); System.out.flush();
-		array = random.clone();
-		System.out.println(Sorting.introSort(array));
-		printArray(array);
-		System.out.println(new String(new char[20]).replace("\0", "-"));
-		
-		// Large test.
+		// Really Large arrays to test usability of algorithms.
+		random = startLargeTest("Huge", 8*1024);
+		for (Sort<Integer> p : fast)
+			testAlgorithmWithArray(
+					p.name,
+					random.clone(),
+					p.algorithm,
+					false
+			);
+	}
+
+	private static Integer[] startLargeTest(String name, int kElements) {
+		String unity = kElements > 1024 ? "M" : "K";
+		int value = kElements > 1024 ? (kElements/1024) : kElements;
+		System.out.println();
+		System.out.println(new String(new char[20]).replace("\0", "="));
+		System.out.println(name + ", "+value+" "+unity+" elements, random array sort.");		
+		System.out.print("Creating array: ");System.out.flush();
 		long s, e;
-		int elements = 64; // K elements.
+		s = System.currentTimeMillis();
+		Integer[] data = new Integer[kElements*1024]; 
+		for (int i = 0; i < kElements*1024; i++)
+			data[i] = (int)(Math.random()*(kElements*1024));
+		e = System.currentTimeMillis();
+		System.out.println(((e-s)/1000.0) + "s");
+		return data;
+	}
 
-		System.out.println("Large, "+elements+"K elements, random sort.");
-		System.out.println("Creating array.");
-		random = new Integer[elements*1024]; 
-		for (int i = 0; i < elements*1024; i++)
-			random[i] = (int)(Math.random()*(elements*1024));
-		
-		System.out.println("\nInsertion Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.insertionSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nBubble Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.bubbleSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nSelection Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.selectionSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nMerge Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.mergeSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nHeap Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.heapSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nQuick Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.quickSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nIntro Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.introSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
+	private static void testAlgorithm(String name, Algorithm<Integer> algorithm) {
+		System.out.println();
+		System.out.println(new String(new char[20]).replace("\0", "="));
+		System.out.println(name + " Sort");
+		testAlgorithmWithArray("ordered", ordered, algorithm, true);
+		testAlgorithmWithArray("reversed", reversed, algorithm, true);
+		testAlgorithmWithArray("random", random, algorithm, true);
+		System.out.println(new String(new char[20]).replace("\0", "="));
+	}
 
+	private static <T> void testAlgorithmWithArray(String style, T[] array, Algorithm <T> algorithm, boolean print) {
+		long s, e;
 		System.out.println(new String(new char[20]).replace("\0", "-"));
-
-		// Really Large test.
-		elements = 8; // M elements.
-		System.out.println("Huge, "+elements+"M elements, random sort.");
-		System.out.println("Creating array.");
-		random = new Integer[elements*1024*1024]; 
-		for (int i = 0; i < elements*1024*1024; i++)
-			random[i] = (int)(Math.random()*(elements*1024*1024));
-
-		System.out.println("\nMerge Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
+		System.out.println("Sorting " + style + ": ");
 		s = System.currentTimeMillis();
-		Sorting.mergeSort(array);
+		T[] clone = array.clone();
 		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nHeap Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
+		System.out.println("Cloning Time: " + ((e-s)/1000.0) + "s");
 		s = System.currentTimeMillis();
-		Sorting.heapSort(array);
+		System.out.println("Operations: " + algorithm.execute(clone));
 		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nQuick Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.quickSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
-		
-		System.out.println("\nIntro Sort");
-		System.out.println("Cloning array.");
-		array = random.clone();
-		System.out.println("Sorting...");
-		s = System.currentTimeMillis();
-		Sorting.introSort(array);
-		e = System.currentTimeMillis();
-		System.out.println("Time: " + ((e-s)/1000) + "s.");
+		System.out.println("Execution Time: " + ((e-s)/1000.0) + "s");
+		if (print)
+			printArray(clone);
 	}
 
 	private static <T> void printArray(T[] array) {
