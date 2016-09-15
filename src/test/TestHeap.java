3f35ded8f566474e31c6c2f48fc87a461e5877a8
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-09-15 12:17:16 -0300

    Adicionado Comaparator aos dois testes de Heap.

--- src/test/TestHeap.java
+++ src/test/TestHeap.java
@@ -15,13 +15,14 @@ import util.BinaryHeap;
 
 public class TestHeap {
 	public static void main(String...strings) {
-		BinaryHeap<Integer> heap = new BinaryHeap<>(new Comparator<Integer>(){
+		Comparator<Integer> heapOrder = new Comparator<Integer>(){
 			@Override
 			public int compare(Integer o1, Integer o2) {
 				//return o1 - o2; // min heap
 				return o2 - o1; // max heap
 			}
-		});
+		};
+		BinaryHeap<Integer> heap = new BinaryHeap<>(heapOrder);
 		
 		for (int i = 0; i < 20; i++)
 			heap.push(i);
@@ -32,7 +33,7 @@ public class TestHeap {
 		
 		System.out.println(new String(new char[20]).replace("\0", "-"));
 		Integer[] array = {14,8,7,2,3,16,15,10,1,9,12,6,13,4,5,11};
-		heap = BinaryHeap.heapify(array);
+		heap = BinaryHeap.heapify(array, heapOrder);
 		while (!heap.isEmpty())
 			System.out.println(heap.pop()+" ");
 		System.out.println();
